8b6c50be8759549133e0e68928f4ef5d0eb32f44
florent-dev <florent84@live.fr>
2024-09-11 15:25:37 +0200

    ✨ [register] autologin, user identity fields, create Client entity linked to JhiUser and Address

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -8,11 +8,17 @@ import { EMAIL_ALREADY_USED_TYPE, LOGIN_ALREADY_USED_TYPE } from 'app/config/err
 import SharedModule from 'app/shared/shared.module';
 import PasswordStrengthBarComponent from '../password/password-strength-bar/password-strength-bar.component';
 import { RegisterService } from './register.service';
+import { AddressService } from '../../entities/address/service/address.service';
+import { IAddress, NewAddress } from '../../entities/address/address.model';
+import { AccountService } from '../../core/auth/account.service';
+import { LoginService } from '../../login/login.service';
+import { ClientService } from '../../entities/client/service/client.service';
+import { Account } from '../../core/auth/account.model';
 
 @Component({
   standalone: true,
   selector: 'jhi-register',
-  imports: [SharedModule, RouterModule, FormsModule, ReactiveFormsModule, PasswordStrengthBarComponent],
+  imports: [SharedModule, FormsModule, ReactiveFormsModule, PasswordStrengthBarComponent, RouterLink],
   templateUrl: './register.component.html',
 })
 export default class RegisterComponent implements AfterViewInit {
@@ -34,6 +40,14 @@ export default class RegisterComponent implements AfterViewInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
+    firstName: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(1), Validators.maxLength(50)],
+    }),
+    lastName: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(1), Validators.maxLength(50)],
+    }),
     email: new FormControl('', {
       nonNullable: true,
       validators: [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email],
@@ -46,10 +60,31 @@ export default class RegisterComponent implements AfterViewInit {
       nonNullable: true,
       validators: [Validators.required, Validators.minLength(4), Validators.maxLength(50)],
     }),
+    street: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    postalCode: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    city: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    country: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
   });
 
   private translateService = inject(TranslateService);
   private registerService = inject(RegisterService);
+  private addressService = inject(AddressService);
+  private accountService = inject(AccountService);
+  private loginService = inject(LoginService);
+  private clientService = inject(ClientService);
+  private router = inject(Router);
 
   ngAfterViewInit(): void {
     this.login().nativeElement.focus();
@@ -65,10 +100,61 @@ export default class RegisterComponent implements AfterViewInit {
     if (password !== confirmPassword) {
       this.doNotMatch.set(true);
     } else {
+      // Identity and address fields.
+      const { firstName, lastName, street, postalCode, city, country } = this.registerForm.getRawValue();
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      const newAddress: NewAddress = { street, postalCode, city, country };
+
+      // Create account.
       const { login, email } = this.registerForm.getRawValue();
-      this.registerService
-        .save({ login, email, password, langKey: this.translateService.currentLang })
-        .subscribe({ next: () => this.success.set(true), error: response => this.processError(response) });
+      this.registerService.save({ login, email, password, langKey: this.translateService.currentLang }).subscribe({
+        next: () => {
+          // Log-in automatically the user.
+          this.loginService.login({ username: login, password, rememberMe: false }).subscribe(() => {
+            this.accountService.identity(true).subscribe({
+              next: (account: Account | null) => {
+                if (account === null) {
+                  this.error.set(true);
+                  return;
+                }
+
+                // Update the account with additional fields.
+                account.firstName = firstName;
+                account.lastName = lastName;
+                this.accountService.save(account).subscribe({
+                  next: () => {
+                    // Create address entity.
+                    this.addressService.create(newAddress).subscribe({
+                      next: res => {
+                        const addressId: IAddress = res.body!;
+
+                        // Create client entity with the user id and address id.
+                        this.clientService
+                          .create({
+                            id: null,
+                            internalUser: { id: account.id! },
+                            address: addressId,
+                          })
+                          .subscribe({
+                            next: () => {
+                              this.router.navigate(['/']);
+                            },
+                            error: response => this.error.set(response),
+                          });
+                      },
+                      error: response => this.error.set(response),
+                    });
+                  },
+                  error: response => this.error.set(response),
+                });
+              },
+              error: response => this.error.set(response),
+            });
+          });
+        },
+        error: response => this.processError(response),
+      });
     }
   }
 
