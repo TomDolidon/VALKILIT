985722ab45f98b33eb38b7a5522078298ff57bd0
Hugo Triolet <95098988+Ogawakin@users.noreply.github.com>
2024-09-12 01:26:18 +0200

    :sparkles: [frontend component] add methods to add/remove

--- src/main/webapp/app/cart/cart.component.ts
+++ src/main/webapp/app/cart/cart.component.ts
@@ -4,7 +4,7 @@ import { Subject } from 'rxjs';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { IPurchaseCommandLine } from 'app/entities/purchase-command-line/purchase-command-line.model';
-
+import { debounce } from 'lodash';
 import SharedModule from 'app/shared/shared.module';
 import { Account } from 'app/core/auth/account.model';
 import { LocalCartService } from './cart.service';
@@ -28,24 +28,29 @@ export default class CartComponent implements OnInit, OnDestroy {
   private router = inject(Router);
   private localCartStorage = inject(LocalCartService);
   private storageKey = 'valkylit-cart';
+  // Debounced version of saveCart()
+  private debouncedSaveCart = debounce(() => {
+    this.localCartStorage.saveCart(this.storageKey, JSON.stringify(this.purchaseLines));
+  }, 500); // 500ms delay
 
   ngOnInit(): void {
     const cartStored = this.localCartStorage.getCart(this.storageKey);
     if (cartStored) {
       this.purchaseLines = JSON.parse(cartStored);
-    } else {
-      this.pushIntoCart({
-        id: '1',
-        quantity: 1,
-        unitPrice: 15.99,
-        book: { id: 'b1', title: "L'art d'avoir toujours raison", price: 15.99 },
-      });
-      this.pushIntoCart({ id: '2', quantity: 1, unitPrice: 20.0, book: { id: 'b2', title: 'Games of Throne : T5', price: 20.0 } });
-      this.pushIntoCart({ id: '3', quantity: 3, unitPrice: 45.36, book: { id: 'b3', title: 'Analyse et Géométrie', price: 45.36 } });
-      this.pushIntoCart({ id: '4', quantity: 1, unitPrice: 7.95, book: { id: 'b4', title: 'One Piece : T106', price: 7.95 } });
-      this.pushIntoCart({ id: '5', quantity: 1, unitPrice: 7.95, book: { id: 'b5', title: 'Berserk : T42', price: 7.95 } });
-      this.pushIntoCart({ id: '6', quantity: 10, unitPrice: 11.99, book: { id: 'b6', title: 'Routard 2024', price: 11.99 } });
     }
+    // else {
+    // this.pushIntoCart({
+    //   id: '1',
+    //   quantity: 1,
+    //   unitPrice: 15.99,
+    //   book: { id: 'b1', title: "L'art d'avoir toujours raison", price: 15.99 },
+    // });
+    // this.pushIntoCart({ id: '2', quantity: 1, unitPrice: 20.0, book: { id: 'b2', title: 'Games of Throne : T5', price: 20.0 } });
+    // this.pushIntoCart({ id: '3', quantity: 3, unitPrice: 45.36, book: { id: 'b3', title: 'Analyse et Géométrie', price: 45.36 } });
+    // this.pushIntoCart({ id: '4', quantity: 1, unitPrice: 7.95, book: { id: 'b4', title: 'One Piece : T106', price: 7.95 } });
+    // this.pushIntoCart({ id: '5', quantity: 1, unitPrice: 7.95, book: { id: 'b5', title: 'Berserk : T42', price: 7.95 } });
+    // this.pushIntoCart({ id: '6', quantity: 10, unitPrice: 11.99, book: { id: 'b6', title: 'Routard 2024', price: 11.99 } });
+    // }
   }
 
   display(): void {
@@ -56,6 +61,18 @@ export default class CartComponent implements OnInit, OnDestroy {
     this.localCartStorage.saveCart(this.storageKey, JSON.stringify(this.purchaseLines));
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  // to add a book in the cart
+  addItemToCart(item: IPurchaseCommandLine): void {
+    this.pushIntoCart(item);
+    this.localCartStorage.saveCart(this.storageKey, JSON.stringify(this.purchaseLines));
+  }
+
+  // to remove a book from the cart
+  removeItemFromCart(index: number): void {
+    this.purchaseLines.splice(index, 1);
+    this.localCartStorage.saveCart(this.storageKey, JSON.stringify(this.purchaseLines));
   }
 
   // to set a basic (test only) cart
