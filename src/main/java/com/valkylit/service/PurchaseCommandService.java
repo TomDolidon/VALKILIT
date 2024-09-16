44da0fdd2ae62fae58c0d15f2f1b34de95b253d8
florent-dev <florent84@live.fr>
2024-09-16 17:31:16 +0200

    🚨 [CI] fix Optional.orElseThrow() issue

--- src/main/java/com/valkylit/service/PurchaseCommandService.java
+++ src/main/java/com/valkylit/service/PurchaseCommandService.java
@@ -13,10 +13,9 @@ import jakarta.persistence.LockTimeoutException;
 import jakarta.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.UUID;
 import org.hibernate.PessimisticLockException;
-import org.hibernate.dialect.lock.OptimisticEntityLockException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,27 +36,6 @@ public class PurchaseCommandService {
 
     @PersistenceContext
     private EntityManager entityManager;
-
-    @Transactional
-    public boolean validateSelfCurrentDraftPurchaseCommand(UUID purchaseCommandId) {
-        try {
-            PurchaseCommand pc = this.entityManager.find(PurchaseCommand.class, purchaseCommandId, LockModeType.READ);
-            pc
-                .getPurchaseCommandLines()
-                .forEach(purchaseCommandLine -> {
-                    Book book = this.entityManager.find(Book.class, purchaseCommandLine.getBook().getId(), LockModeType.WRITE);
-                    int futureStock = book.getStock() - purchaseCommandLine.getQuantity();
-                    if (futureStock >= 0) {
-                        book.setStock(futureStock);
-                        this.bookRepository.save(book);
-                    }
-                });
-            return true;
-        } catch (OptimisticEntityLockException | LockTimeoutException e) {
-            // doing something
-            return false;
-        }
-    }
 
     public List<PurchaseCommandInvalidLineDTO> getInvalidBooksStockForSelfCurrentDraftPurchaseCommand() throws Exception {
         String userLogin = SecurityUtils.getCurrentUserLogin().orElseThrow(() -> new Exception("Current user login not found"));
@@ -86,29 +64,24 @@ public class PurchaseCommandService {
     @Transactional
     public boolean validateSelfCurrentDraftPurchaseCommand() throws Exception {
         String userLogin = SecurityUtils.getCurrentUserLogin().orElseThrow(() -> new Exception("Current user login not found"));
-        Optional<PurchaseCommand> purchaseCommand = purchaseCommandRepository.findCurrentDraftByLogin(userLogin);
-        if (purchaseCommand.isEmpty()) {
-            throw new Exception("No purchase command found");
-        }
+        Optional<PurchaseCommand> purchaseCommandOptional = purchaseCommandRepository.findCurrentDraftByLogin(userLogin);
+        PurchaseCommand purchaseCommand = purchaseCommandOptional.orElseThrow(() -> new NoSuchElementException("PurchaseCommand not found")
+        );
 
         for (int i = 0; i < PURCHASE_ATTEMPTS_COUNT; i++) {
             try {
                 purchaseCommand
-                    .get()
                     .getPurchaseCommandLines()
                     .forEach(pcl -> {
                         Book book = entityManager.find(Book.class, pcl.getBook().getId(), LockModeType.PESSIMISTIC_WRITE);
                         int futureStock = book.getStock() - pcl.getQuantity();
                         if (futureStock < 0) {
-                            try {
-                                throw new Exception("Insufficient stock");
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
+                            throw new RuntimeException("Insufficient stock");
                         }
                         book.setStock(futureStock);
                     });
-                purchaseCommand.get().setStatus(PurchaseCommandStatus.ORDERED);
+
+                purchaseCommand.setStatus(PurchaseCommandStatus.ORDERED);
                 return true;
             } catch (PessimisticLockException | LockTimeoutException e) {
                 // Don't throw anything. Let's try on another attempt.
