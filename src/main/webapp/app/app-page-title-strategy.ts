0008a2bee41c30a1c45d0767a50bbb502917f5b5
Ogawakin <htriolet@ymail.com>
2024-09-18 16:42:20 +0200

    :bug: [UI] Display app's name in tabs
    BONUS : fix number of available book references in home

--- src/main/webapp/app/app-page-title-strategy.ts
+++ src/main/webapp/app/app-page-title-strategy.ts
@@ -7,10 +7,11 @@ export class AppPageTitleStrategy extends TitleStrategy {
   private readonly translateService = inject(TranslateService);
 
   override updateTitle(routerState: RouterStateSnapshot): void {
-    let pageTitle = this.buildTitle(routerState);
-    if (!pageTitle) {
-      pageTitle = 'global.title';
-    }
+    // let pageTitle = this.buildTitle(routerState);
+    // if (!pageTitle) {
+    //   pageTitle = 'global.title';
+    // }
+    const pageTitle = 'global.title';
     this.translateService.get(pageTitle).subscribe(title => {
       document.title = title;
     });
