3a56b40645fd4bf3c0d11a25ab270fc22c0c26ec
tom.dolidon <tom.dolidon@smart-origin.com>
2024-09-18 09:43:09 +0200

    🚑️ [backend test] fix throwing exceptions

--- src/main/java/com/valkylit/service/ReviewService.java
+++ src/main/java/com/valkylit/service/ReviewService.java
@@ -2,32 +2,15 @@ package com.valkylit.service;
 
 import com.valkylit.domain.Book;
 import com.valkylit.domain.Client;
-import com.valkylit.domain.PurchaseCommand;
-import com.valkylit.domain.PurchaseCommandLine;
 import com.valkylit.domain.Review;
-import com.valkylit.domain.enumeration.PurchaseCommandStatus;
 import com.valkylit.repository.BookRepository;
-import com.valkylit.repository.PurchaseCommandLineRepository;
-import com.valkylit.repository.PurchaseCommandRepository;
 import com.valkylit.repository.ReviewRepository;
-import com.valkylit.security.SecurityUtils;
-import com.valkylit.service.dto.PurchaseCommandInvalidLineDTO;
 import com.valkylit.service.dto.ReviewDTO;
 import com.valkylit.web.rest.errors.BadRequestAlertException;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.LockModeType;
-import jakarta.persistence.LockTimeoutException;
-import jakarta.persistence.PersistenceContext;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-import org.hibernate.PessimisticLockException;
-import org.hibernate.dialect.lock.OptimisticEntityLockException;
-import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReviewService {
@@ -38,11 +21,9 @@ public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
-    public Review createReview(Client client, UUID bookId, ReviewDTO reviewDTO) {
+    public Review createReview(Client client, UUID bookId, ReviewDTO reviewDTO) throws Exception {
         // Récupérer le livre par son ID
-        Book book = bookRepository
-            .findById(bookId)
-            .orElseThrow(() -> new BadRequestAlertException("Book not found", "book", "bookNotFound"));
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new Exception("Book not found"));
 
         // Créer une nouvelle review
         Review review = new Review();
